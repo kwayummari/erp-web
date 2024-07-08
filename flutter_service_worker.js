8f97456287f5491346f5c80448df3a993e1ae33a
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-08 15:12:21 +0300

    fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "964d2bb1500f418307a6580124d40adf",
+const RESOURCES = {"flutter_bootstrap.js": "81af7b43eadecba3effc635f6e407a1d",
 "version.json": "6a7edee948f67f06a99d8821daef3c6f",
 "index.html": "9966cf320f12a48f0fcf3a7423c1d0e0",
 "/": "9966cf320f12a48f0fcf3a7423c1d0e0",
-"main.dart.js": "ae9daf82d44fe2cae8f525fc1a47e21b",
+"main.dart.js": "325bd7008cead27e3bfcef2875594f6e",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
